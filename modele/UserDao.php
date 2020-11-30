864ad200f3bede27970f1829eff7f5d623b0d887
e191487n <corentin.pinguet@etu.univ-nantes.fr>
2020-11-30 17:12:48 +0100

    Fausse doc retirée

--- modele/UserDao.php
+++ modele/UserDao.php
@@ -9,38 +9,11 @@ class UserDao
     // ajouter le(s) attribut(s)
     private $connexion;
 
-
-    /** le constructeur de la classe
-     */
     public function __construct()
     {
         $this->connexion = SqliteConnexion::getInstance()->getConnexion();
     }
 
-    /**
-     * méthode qui retourne un tableau de User avec le pseudo renseigné
-     * @return tabeau de User avec le pseudo renseigné
-     * @throws TableAccesException si la requête SQL pose problème
-     */
-    public function findAllByPseudo(): array
-    {
-        try {
-            $statement = $this->connexion->query("SELECT pseudo from JOUEURS;");
-            $users = $statement->fetchAll(PDO::FETCH_CLASS, 'User');
-            return ($users);
-        } catch (PDOException $e) {
-            throw new SQLException("problème requête SQL sur la table utilisateurs");
-
-
-        }
-    }
-
-    /**
-     * méthode qui détermine si un utilisateur avec un certain pseudo est dans la table pseudonyme
-     * @param $pseudo un pseudo
-     * @return true si le pseudo passé en pramètre correspond à un utilisateur dans la table utilisateur, false sinon
-     * @throws TableAccesException si la requête SQL pose problème
-     */
     public function exists(string $pseudo): bool
     {
         try {
