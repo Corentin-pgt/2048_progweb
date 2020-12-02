730a6ae35116964d60c664c2d014f0c6b0148de9
e191487n <corentin.pinguet@etu.univ-nantes.fr>
2020-12-02 15:11:20 +0100

    Fix direction grille + apparition

--- controleur/ControleurJeu.php
+++ controleur/ControleurJeu.php
@@ -43,36 +43,37 @@ class ControleurJeu
                 $this->vue->game();
             } else {
                 $l = 0;
+                $grille = $_SESSION["grille"];
 
                 switch ($direction) {
                     case "haut":
-                        $k1 = $this->retasseHaut($_SESSION["grille"]);
-                        $k2 = $this->additionneHaut($_SESSION["grille"]);
-                        $k3 = $this->retasseHaut($_SESSION["grille"]);
+                        $k1 = $this->retasseHaut($grille);
+                        $k2 = $this->additionneHaut($grille);
+                        $k3 = $this->retasseHaut($grille);
                         $id = $this->gameDAO->inGame();
                         $this->gameDAO->setScore($id, ($this->gameDAO->getScore($id) + $k2));
                         $l = $k1 + $k2 + $k3;
                         break;
                     case "gauche":
-                        $k1 = $this->retasseGauche($_SESSION["grille"]);
-                        $k2 = $this->additionneGauche($_SESSION["grille"]);
-                        $k3 = $this->retasseGauche($_SESSION["grille"]);
+                        $k1 = $this->retasseGauche($grille);
+                        $k2 = $this->additionneGauche($grille);
+                        $k3 = $this->retasseGauche($grille);
                         $id = $this->gameDAO->inGame();
                         $this->gameDAO->setScore($id, ($this->gameDAO->getScore($id) + $k2));
                         $l = $k1 + $k2 + $k3;
                         break;
                     case "bas":
-                        $k1 = $this->retasseBas($_SESSION["grille"]);
-                        $k2 = $this->additionnebas($_SESSION["grille"]);
-                        $k3 = $this->retasseBas($_SESSION["grille"]);
+                        $k1 = $this->retasseBas($grille);
+                        $k2 = $this->additionnebas($grille);
+                        $k3 = $this->retasseBas($grille);
                         $id = $this->gameDAO->inGame();
                         $this->gameDAO->setScore($id, ($this->gameDAO->getScore($id) + $k2));
                         $l = $k1 + $k2 + $k3;
                         break;
                     case "droite":
-                        $k1 = $this->retasseDroite($_SESSION["grille"]);
-                        $k2 = $this->additionneDroite($_SESSION["grille"]);
-                        $k3 = $this->retasseDroite($_SESSION["grille"]);
+                        $k1 = $this->retasseDroite($grille);
+                        $k2 = $this->additionneDroite($grille);
+                        $k3 = $this->retasseDroite($grille);
                         $id = $this->gameDAO->inGame();
                         $this->gameDAO->setScore($id, ($this->gameDAO->getScore($id) + $k2));
                         $l = $k1 + $k2 + $k3;
@@ -83,7 +84,7 @@ class ControleurJeu
                     $cpt = 0;
                     for ($row = 0; $row < 4; $row++) {
                         for ($col = 0; $col < 4; $col++) {
-                            if ($_SESSION["grille"][$row][$col] == "") {
+                            if ($grille[$row][$col] == "") {
                                 $dispo[$cpt][0] = $row;
                                 $dispo[$cpt][1] = $col;
                                 $cpt++;
@@ -97,7 +98,7 @@ class ControleurJeu
                         $row_random = random_int(0, sizeof($dispo));
                         $col_random = random_int(0, sizeof($dispo));
                         $value_random = random_int(1, 2);
-                        $dispo[$row_random][$col_random] = $value_random * 2;
+                        $grille[$dispo[$row_random]][$dispo[$col_random]] = $value_random * 2;
                     } catch (Exception $e) {
                     }
                 }
@@ -122,6 +123,7 @@ class ControleurJeu
                     $k--;
                 }
         }
+        $_SESSION["grille"] = $grille;
         return $l;
     }
 
@@ -140,6 +142,7 @@ class ControleurJeu
                     $k++;
                 }
         }
+        $_SESSION["grille"] = $grille;
         return $l;
     }
 
@@ -158,6 +161,7 @@ class ControleurJeu
                     $k++;
                 }
         }
+        $_SESSION["grille"] = $grille;
         return $l;
     }
 
@@ -176,58 +180,71 @@ class ControleurJeu
                     $k--;
                 }
         }
+        $_SESSION["grille"] = $grille;
         return $l;
     }
 
     private function additionneDroite($grille)
     {
         $score = 0;
-        for ($i = 0; $i < 4; $i++)
-            for ($j = 3; $j > 0; $j--)
+        for ($i = 0; $i < 4; $i++) {
+            for ($j = 3; $j > 0; $j--) {
                 if ($grille[$i][$j] == $grille[$i][$j - 1]) {
                     $grille[$i][$j] = $grille[$i][$j] + $grille[$i][$j - 1];
                     $score += $grille[$i][$j];
                     $grille[$i][$j - 1] = 0;
                 }
+            }
+        }
+        $_SESSION["grille"] = $grille;
         return $score;
     }
 
     private function additionneGauche($grille)
     {
         $score = 0;
-        for ($i = 0; $i < 4; $i++)
-            for ($j = 0; $j < 3; $j++)
+        for ($i = 0; $i < 4; $i++) {
+            for ($j = 0; $j < 3; $j++) {
                 if ($grille[$i][$j] == $grille[$i][$j + 1]) {
                     $grille[$i][$j] = $grille[$i][$j] + $grille[$i][$j + 1];
                     $score += $grille[$i][$j];
                     $grille[$i][$j + 1] = 0;
                 }
+            }
+        }
+        $_SESSION["grille"] = $grille;
         return $score;
     }
 
     private function additionneHaut($grille)
     {
         $score = 0;
-        for ($i = 0; $i < 3; $i++)
-            for ($j = 0; $j < 4; $j++)
+        for ($i = 0; $i < 3; $i++) {
+            for ($j = 0; $j < 4; $j++) {
                 if ($grille[$i][$j] == $grille[$i + 1][$j]) {
                     $grille[$i][$j] = $grille[$i][$j] + $grille[$i + 1][$j];
                     $score += $grille[$i][$j];
                     $grille[$i + 1][$j] = 0;
                 }
+            }
+        }
+        $_SESSION["grille"] = $grille;
         return $score;
     }
 
     private function additionnebas($grille)
     {
         $score = 0;
-        for ($i = 3; $i > 0; $i--)
-            for ($j = 0; $j < 4; $j++)
+        for ($i = 3; $i > 0; $i--) {
+            for ($j = 0; $j < 4; $j++) {
                 if ($grille[$i][$j] == $grille[$i - 1][$j]) {
                     $grille[$i][$j] = $grille[$i][$j] + $grille[$i - 1][$j];
                     $score += $grille[$i][$j];
                     $grille[$i - 1][$j] = 0;
                 }
+            }
+        }
+        $_SESSION["grille"]=$grille;
         return $score;
     }
 
