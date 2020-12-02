56ece7da1f4fe58411682befac0b5a70af09186c
Lucas VIGIER <lucas.vigier@etu.univ-nantes.fr>
2020-12-02 09:28:48 +0100

    style login

--- vue/Vue.php
+++ vue/Vue.php
@@ -7,29 +7,22 @@ class Vue
     {
         ?>
         <!DOCTYPE html>
-        <html lang="fr">
+        <html>
         <head>
+            <meta charset="utf-8">
             <title>2048: Le jeu du futur </title>
+            <link rel="stylesheet" content="text/css" href="css/style.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet">
         </head>
         <body>
-        <br>
-        <br>
-        <form action="index.php" method="post">
-            <label for="pseudo">Entrer votre pseudo : </label>
-            <br>
-            <input type="text" name="pseudo" id="pseudo" required>
-            <br>
-            <br>
-            <label for="pwd">Entrer votre mot de passe : </label>
-            <br>
-            <input type="password" name="pwd" id="pwd" required>
-            <br>
-            <br>
-            <input type="submit" name="inscription" value="inscription"/>
-            <input type="submit" name="connexion" value="connexion"/>
-        </form>
-        <br>
-        <br>
+            <form class="login" action="index.php" method="post">
+                <input type="text" name="pseudo" id="pseudo" placeholder="Username" required>
+                <input type="password" name="pwd" id="pwd" placeholder="Password" required>
+                <input type="submit" name="connexion" value="Login" class="button"/>
+                <input type="submit" name="inscription" value="Sign in" class="button"/>
+            </form>
         </body>
         </html>
         <?php
@@ -39,12 +32,25 @@ class Vue
     {
         ?>
         <!DOCTYPE html>
-        <html lang="fr">
+        <html>
         <head>
+            <meta charset="utf-8">
             <title>2048: Le jeu ! </title>
+            <link rel="stylesheet" content="text/css" href="css/style.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet">
         </head>
         <body>
-
+            <?php
+            $grille = $_SESSION["grille"];
+            ?>
+            <div class="grid">
+                <div class="row1"><?php echo($grille[0][0]) ?></div>
+                <div class="row2"></div>
+                <div class="row3"></div>
+                <div class="row4"></div>
+            </div>
         </body>
         </html>
         <?php
@@ -54,7 +60,7 @@ class Vue
     {
         ?>
         <!DOCTYPE html>
-        <html lang="fr">
+        <html>
         <head>
             <title>2048: Résultats </title>
         </head>
