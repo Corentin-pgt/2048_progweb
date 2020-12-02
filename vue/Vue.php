d0c3467d0924c5fd5c70c7e9e24367bb557a3719
Lucas VIGIER <lucas.vigier@etu.univ-nantes.fr>
2020-12-02 11:11:43 +0100

    score and pseudo

--- vue/Vue.php
+++ vue/Vue.php
@@ -11,18 +11,20 @@ class Vue
         <head>
             <meta charset="utf-8">
             <title>2048: Le jeu du futur </title>
+            <link rel="icon" href="assets/icon.png">
             <link rel="stylesheet" content="text/css" href="css/style.css">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet">
         </head>
         <body>
-            <form class="login" action="index.php" method="post">
-                <input type="text" name="pseudo" id="pseudo" placeholder="Username" required>
-                <input type="password" name="pwd" id="pwd" placeholder="Password" required>
-                <input type="submit" name="connexion" value="Login" class="button"/>
-                <input type="submit" name="inscription" value="Sign in" class="button"/>
-            </form>
+        <center><img src="assets/icon.png"></center>
+        <form class="login" action="index.php" method="post">
+            <input type="text" name="pseudo" id="pseudo" placeholder="Username" required>
+            <input type="password" name="pwd" id="pwd" placeholder="Password" required>
+            <input type="submit" name="connexion" value="Login" class="button"/>
+            <input type="submit" name="inscription" value="Sign in" class="button"/>
+        </form>
         </body>
         </html>
         <?php
@@ -36,6 +38,7 @@ class Vue
         <head>
             <meta charset="utf-8">
             <title>2048: Le jeu ! </title>
+            <link rel="icon" href="assets/icon.png">
             <link rel="stylesheet" content="text/css" href="css/style.css">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
             <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -44,31 +47,41 @@ class Vue
         <body>
             <?php
             $grille = $_SESSION["grille"];
+            $pseudo = $_SESSION["pseudo"];
+            $score = $_SESSION["score"];
             ?>
-            <div class="grid">
-                <div class="row1">
-                    <div class="col1"><?php echo($grille[0][0]); ?></div>
-                    <div class="col2"><?php echo($grille[0][1]); ?></div>
-                    <div class="col3"><?php echo($grille[0][2]); ?></div>
-                    <div class="col4"><?php echo($grille[0][3]); ?></div>
+            <div class="content">
+                <div class="grid">
+                    <div class="row1">
+                        <div class="col1"><p><?php echo($grille[0][0]); ?></p></div>
+                        <div class="col2"><p><?php echo($grille[0][1]); ?></p></div>
+                        <div class="col3"><p><?php echo($grille[0][2]); ?></p></div>
+                        <div class="col4"><p><?php echo($grille[0][3]); ?></p></div>
+                    </div>
+                    <div class="row2">
+                        <div class="col1"><p><?php echo($grille[1][0]); ?></p></div>
+                        <div class="col2"><p><?php echo($grille[1][1]); ?></p></div>
+                        <div class="col3"><p><?php echo($grille[1][2]); ?></p></div>
+                        <div class="col4"><p><?php echo($grille[1][3]); ?></p></div>
+                    </div>
+                    <div class="row3">
+                        <div class="col1"><p><?php echo($grille[2][0]); ?></p></div>
+                        <div class="col2"><p><?php echo($grille[2][1]); ?></p></div>
+                        <div class="col3"><p><?php echo($grille[2][2]); ?></p></div>
+                        <div class="col4"><p><?php echo($grille[2][3]); ?></p></div>
+                    </div>
+                    <div class="row4">
+                        <div class="col1"><p><?php echo($grille[3][0]); ?></p></div>
+                        <div class="col2"><p><?php echo($grille[3][1]); ?></p></div>
+                        <div class="col3"><p><?php echo($grille[3][2]); ?></p></div>
+                        <div class="col4"><p><?php echo($grille[3][3]); ?></p></div>
+                    </div>
                 </div>
-                <div class="row2">
-                    <div class="col1"><?php echo($grille[1][0]); ?></div>
-                    <div class="col2"><?php echo($grille[1][1]); ?></div>
-                    <div class="col3"><?php echo($grille[1][2]); ?></div>
-                    <div class="col4"><?php echo($grille[1][3]); ?></div>
+                <div class="pseudo">
+                    <p><strong><i class="fas fa-user"></i> : </strong><?php echo($pseudo) ?></p>
                 </div>
-                <div class="row3">
-                    <div class="col1"><?php echo($grille[2][0]); ?></div>
-                    <div class="col2"><?php echo($grille[2][1]); ?></div>
-                    <div class="col3"><?php echo($grille[2][2]); ?></div>
-                    <div class="col4"><?php echo($grille[2][3]); ?></div>
-                </div>
-                <div class="row4">
-                    <div class="col1"><?php echo($grille[3][0]); ?></div>
-                    <div class="col2"><?php echo($grille[3][1]); ?></div>
-                    <div class="col3"><?php echo($grille[3][2]); ?></div>
-                    <div class="col4"><?php echo($grille[3][3]); ?></div>
+                <div class="score">
+                    <p><strong><i class="fas fa-star"></i> : </strong><?php echo($score) ?></p>
                 </div>
             </div>
         </body>
