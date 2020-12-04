43a7b34dc5eab10bd5f40eb631da7fcd14f89735
Lucas VIGIER <lucas.vigier@etu.univ-nantes.fr>
2020-12-04 11:13:08 +0100

    results

--- vue/Vue.php
+++ vue/Vue.php
@@ -188,7 +188,13 @@ class Vue
         <!DOCTYPE html>
         <html>
         <head>
+            <meta charset="utf-8">
             <title>2048: Résultats </title>
+            <link rel="icon" href="assets/icon.png">
+            <link rel="stylesheet" content="text/css" href="css/style.css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet">
         </head>
         <body>
 
